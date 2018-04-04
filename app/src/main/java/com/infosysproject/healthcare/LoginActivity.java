3ed8274ed472590f17ffcb1edf3a2647dc334f36
Bharth V <bharathvaradaraj96@gmail.com>
2018-04-04 20:46:44 +0530

    Added HomePage

--- app/src/main/java/com/infosysproject/healthcare/LoginActivity.java
+++ app/src/main/java/com/infosysproject/healthcare/LoginActivity.java
@@ -2,12 +2,28 @@ package com.infosysproject.healthcare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText user, password;
+    private Button login_bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        user = findViewById(R.id.userid);
+        password = findViewById(R.id.password);
+        login_bt = findViewById(R.id.login_bt);
+
+        login_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/infosysproject/healthcare/Patient_HomePage.java
@@ -0,0 +1,13 @@
+package com.infosysproject.healthcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Patient_HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient__home_page);
+    }
+}
